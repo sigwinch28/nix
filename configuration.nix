3f6fbd9acb9dca9cc56d76c4145bbb7b60cd6ec8
Joe Harrison <joe@sigwinch.uk>
2023-03-07 01:08:18 +0000

    fix nix.conf

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,31 @@
 { pkgs, ... }:
 {
-  nix.package = pkgs.nixFlakes;
-  nix.useDaemon = true;
+  nix = {
+    package = pkgs.nixFlakes;
+    useDaemon = true;
+    settings = {
+      auto-optimise-store = true;
+    };
+      
+    extraOptions = ''
+      extra-nix-path = nixpkgs=flake:nixpkgs
+      experimental-features = nix-command flakes
+      bash-prompt-prefix = (nix:$name)\040
+    '';
+  };
 
   programs.bash.enable = false;
 
-  homebrew = {
-    enable = true;
-
-    onActivation = {
-      autoUpdate = true;
-      upgrade = true;
-    };
-
-    casks = [ ];
-  };
+  #homebrew = {
+  #  enable = true;
+  #
+  #  onActivation = {
+  #    autoUpdate = true;
+  #    upgrade = true;
+  #  };
+  #
+  #  casks = [ ];
+  #};
 
   users.users.joe = {
     home = "/Users/joe";
